b7b2d9aa88a4ff46bc76a7deec2b36ae0af9b11f
quy <quy1403a2@gmail.com>
2024-08-03 16:19:05 +0700

    1

--- build/web-mobile/index.ad6d2.js
+++ build/web-mobile/index.ad6d2.js
@@ -1,4 +1,4 @@
-System.register(["./application.7aef7.js"], function (_export, _context) {
+System.register(["./application.1a4e3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -6,8 +6,8 @@ System.register(["./application.7aef7.js"], function (_export, _context) {
     return System["import"](url);
   }
   return {
-    setters: [function (_application7aef7Js) {
-      Application = _application7aef7Js.Application;
+    setters: [function (_application1a4e3Js) {
+      Application = _application1a4e3Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
