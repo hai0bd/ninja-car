907fc33eb84266b31267a46fa343ed3a49197695
hai0bd <dosonhai2004@gmail.com>
2024-08-03 18:18:57 +0700

    car steer 1.1

--- build/web-mobile/index.3811f.js
+++ build/web-mobile/index.3811f.js
@@ -1,4 +1,4 @@
-System.register(["./application.1a4e3.js"], function (_export, _context) {
+System.register(["./application.76d08.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -6,8 +6,8 @@ System.register(["./application.1a4e3.js"], function (_export, _context) {
     return System["import"](url);
   }
   return {
-    setters: [function (_application1a4e3Js) {
-      Application = _application1a4e3Js.Application;
+    setters: [function (_application76d08Js) {
+      Application = _application76d08Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
