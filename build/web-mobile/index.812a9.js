508fa148d7da387e54023dcca262cb97875658da
hai0bd <dosonhai2004@gmail.com>
2024-08-05 09:31:24 +0700

    update version 1.0.2 -1

--- build/web-mobile/index.812a9.js
+++ build/web-mobile/index.812a9.js
@@ -1,4 +1,4 @@
-System.register(["./application.76d08.js"], function (_export, _context) {
+System.register(["./application.0a684.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -6,8 +6,8 @@ System.register(["./application.76d08.js"], function (_export, _context) {
     return System["import"](url);
   }
   return {
-    setters: [function (_application76d08Js) {
-      Application = _application76d08Js.Application;
+    setters: [function (_application0a684Js) {
+      Application = _application0a684Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
