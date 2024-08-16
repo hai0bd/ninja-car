71273b04c3db6f015ea683f4afc5ff0c54971642
hai0bd <dosonhai2004@gmail.com>
2024-08-16 09:37:12 +0700

    update verision 1.0.7

--- build/web-mobile/index.20d9b.js
+++ build/web-mobile/index.20d9b.js
@@ -1,4 +1,4 @@
-System.register(["./application.0337d.js"], function (_export, _context) {
+System.register(["./application.54680.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -6,8 +6,8 @@ System.register(["./application.0337d.js"], function (_export, _context) {
     return System["import"](url);
   }
   return {
-    setters: [function (_applicationJs) {
-      Application = _applicationJs.Application;
+    setters: [function (_application54680Js) {
+      Application = _application54680Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
